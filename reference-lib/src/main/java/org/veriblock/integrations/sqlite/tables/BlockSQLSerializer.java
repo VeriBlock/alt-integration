d2220fdd256f2b0c219d968d5c6a35e87e481114
Eugene Iegorochkin <eugene_iegorochkin@adoriasoft.com>
2019-10-25 10:16:37 +0300

    BlockSQLSerializer: document the interface

--- reference-lib/src/main/java/org/veriblock/integrations/sqlite/tables/BlockSQLSerializer.java
+++ reference-lib/src/main/java/org/veriblock/integrations/sqlite/tables/BlockSQLSerializer.java
@@ -14,9 +14,19 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface BlockSQLSerializer<Block, Id> {
+
+    // populate a prepared statement with block data
     void toStmt(Block block, PreparedStatement stmt) throws SQLException;
+
+    // convert an SQL row to a block
     Block fromResult(ResultSet result)  throws SQLException;
+
+    // get the SQL database schema to use to store block data
     String getSchema();
+
+    // get the list of column names that match the schema
     List<String> getColumns();
+
+    // convert an id to a string
     String idToString(Id id);
 }
