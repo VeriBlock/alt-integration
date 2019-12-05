300d4c8d1fd9d327919daff1f2ce3956b7f18ff0
Mr-Leshiy <alex_pozhilenkov@adoriasoft.com>
2019-12-05 19:14:06 +0200

    add new PopService methods (#80)

--- alt-integration-adapter-java/src/main/java/org/veriblock/protoservice/PopServiceProto.java
+++ alt-integration-adapter-java/src/main/java/org/veriblock/protoservice/PopServiceProto.java
@@ -1,20 +1,22 @@
 package org.veriblock.protoservice;
 
+import com.google.protobuf.ByteString;
 import integration.api.grpc.VeriBlockMessages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.veriblock.protoconverters.AltChainBlockProtoConverter;
 import org.veriblock.protoconverters.RewardOutputProtoConverter;
+import org.veriblock.sdk.Context;
 import org.veriblock.sdk.VeriBlockSecurity;
-import org.veriblock.sdk.models.AltChainBlock;
-import org.veriblock.sdk.models.AltPublication;
-import org.veriblock.sdk.models.ValidationResult;
-import org.veriblock.sdk.models.VeriBlockPublication;
+import org.veriblock.sdk.forkresolution.ForkresolutionComparator;
+import org.veriblock.sdk.models.*;
 import org.veriblock.sdk.rewards.PopPayoutRound;
 import org.veriblock.sdk.rewards.PopRewardCalculator;
 import org.veriblock.sdk.services.SerializeDeserializeService;
+import org.veriblock.sdk.sqlite.tables.PoPTransactionData;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -42,8 +44,7 @@ public class PopServiceProto {
         return VeriBlockServiceCommon.validationResultToCheckReplyProto(result);
     }
 
-    static public VeriBlockMessages.RewardsCalculateReply rewardsCalculateOutputs(VeriBlockMessages.RewardsCalculateRequest request) throws Exception
-    {
+    static public VeriBlockMessages.RewardsCalculateReply rewardsCalculateOutputs(VeriBlockMessages.RewardsCalculateRequest request) throws Exception {
         AltChainBlock endorsedBlock = AltChainBlockProtoConverter.fromProto(request.getEndorsedBlock());
         List<AltChainBlock> endorsmentBlocks = AltChainBlockProtoConverter.fromProto(request.getEndorsmentBlocksList());
         List<AltChainBlock> difficultyBlocks = AltChainBlockProtoConverter.fromProto(request.getDifficultyBlocksList());
@@ -55,6 +56,53 @@ public class PopServiceProto {
                 .addAllOutputs(RewardOutputProtoConverter.toProto(payout.getOutputsToPopMiners()))
                 .setBlockReward(Long.toString(payout.getPopBlockReward()))
                 .setTotalReward(Long.toString(payout.getTotalRewardPaidOut()))
+                .build();
+    }
+
+    static public VeriBlockMessages.EmptyReply saveBlockPopTxToDatabase(VeriBlockMessages.SaveBlockPopTxRequest request) throws Exception {
+        AltChainBlock containingBlock = AltChainBlockProtoConverter.fromProto(request.getContainingBlock());
+        for(VeriBlockMessages.PopTxData popData : request.getPopDataList())
+        {
+            AltChainBlock endorsedBlock = AltChainBlockProtoConverter.fromProto(popData.getEndorsedBlock());
+            AltPublication altPublication = SerializeDeserializeService.parseAltPublication(popData.getAltPublication().toByteArray());
+
+            List<VeriBlockPublication> veriBlockPublications = new ArrayList<VeriBlockPublication>();
+            for(ByteString veriBlockPublicationBytes : popData.getVeriblockPublicationsList())
+            {
+                veriBlockPublications.add(SerializeDeserializeService.parseVeriBlockPublication(veriBlockPublicationBytes.toByteArray()));
+            }
+
+            PoPTransactionData popTx = new PoPTransactionData(popData.getPopTxHash(), altPublication, veriBlockPublications);
+            Context.getPopTxDBStore().addPoPTransaction(popTx, containingBlock, endorsedBlock);
+        }
+
+        return VeriBlockMessages.EmptyReply.newBuilder().build();
+    }
+
+    public static VeriBlockMessages.EmptyReply updateContext(VeriBlockMessages.UpdateContextRequest request) throws Exception {
+        List<BitcoinBlock> bitcoinBlocks = new ArrayList<BitcoinBlock>(request.getBitcoinBlocksCount());
+        List<VeriBlockBlock> veriBlockBlocks = new ArrayList<VeriBlockBlock>(request.getVeriBlockBlocksCount());
+        for(ByteString encodedBlock : request.getBitcoinBlocksList()) {
+            BitcoinBlock block = SerializeDeserializeService.parseBitcoinBlock(encodedBlock.toByteArray());
+            bitcoinBlocks.add(block);
+        }
+
+        for(ByteString encodedBlock : request.getVeriBlockBlocksList()) {
+            VeriBlockBlock block = SerializeDeserializeService.parseVeriBlockBlock(encodedBlock.toByteArray());
+            veriBlockBlocks.add(block);
+        }
+
+        security.updateContext(bitcoinBlocks, veriBlockBlocks);
+        return VeriBlockMessages.EmptyReply.newBuilder().build();
+    }
+
+    public static VeriBlockMessages.CompareTwoBranchesReply compareTwoBranches(VeriBlockMessages.TwoBranchesRequest request) throws Exception {
+        List<AltChainBlock> leftFork = AltChainBlockProtoConverter.fromProto(request.getLeftForkList());
+        List<AltChainBlock> rightFork = AltChainBlockProtoConverter.fromProto(request.getRightForkList());
+        int compareResult = ForkresolutionComparator.compareTwoBranches(leftFork, rightFork);
+
+        return VeriBlockMessages.CompareTwoBranchesReply.newBuilder()
+                .setCompareResult(compareResult)
                 .build();
     }
 }
