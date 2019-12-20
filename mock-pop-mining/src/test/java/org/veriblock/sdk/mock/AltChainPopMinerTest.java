db016e26e3f399357a232cb24a5b672de0474407
eu321 <52827508+eu321@users.noreply.github.com>
2019-12-20 16:04:48 +0200

    create mock-pop-mining subproject (#95)

---
+++ mock-pop-mining/src/test/java/org/veriblock/sdk/mock/AltChainPopMinerTest.java
@@ -0,0 +1,33 @@
+// VeriBlock Blockchain Project
+// Copyright 2017-2018 VeriBlock, Inc
+// Copyright 2018-2019 Xenios SEZC
+// All rights reserved.
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+package org.veriblock.sdk.mock;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AltChainPopMinerTest {
+
+    private AltChainPopMiner apm;
+
+    @Before
+    public void setUp() {
+        apm = new AltChainPopMiner();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+}
