6df229c0a6f73a89946d853b9f995c4f099215f0
Bohdan Vanieiev <warchantua@gmail.com>
2019-12-27 18:14:13 +0200

    Fix AuditorChangesRepository

--- core/src/main/java/org/veriblock/sdk/sqlite/tables/AuditorChangesRepository.java
+++ core/src/main/java/org/veriblock/sdk/sqlite/tables/AuditorChangesRepository.java
@@ -23,9 +23,7 @@ public class AuditorChangesRepository {
 
     public AuditorChangesRepository(Connection connection) throws SQLException {
         this.connectionSource = connection;
-        Statement stmt = null;
-        try {
-            stmt = connectionSource.createStatement();
+        try (Statement stmt = connectionSource.createStatement()) {
             stmt.execute("CREATE TABLE IF NOT EXISTS tableAuditorChanges (\n"
                     + " id INTEGER PRIMARY KEY,\n"
                     + " blockId TEXT NOT NULL,\n"
@@ -35,37 +33,24 @@ public class AuditorChangesRepository {
                     + " oldValue TEXT,\n"
                     + " newValue TEXT\n"
                     + ");");
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
+
         }
-        
-        try {
-            stmt = connectionSource.createStatement();
+
+        try (Statement stmt = connectionSource.createStatement()) {
             stmt.execute("PRAGMA journal_mode=WAL;");
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
     }
-    
+
     public void clear() throws SQLException {
-        Statement stmt = null;
-        try {
-            stmt = connectionSource.createStatement();
+        try (Statement stmt = connectionSource.createStatement()) {
             stmt.execute("DELETE FROM tableAuditorChanges");
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
     }
 
     public void save(AuditorChangeData change) throws SQLException {
-        PreparedStatement stmt = null;
-        try {
-            stmt = connectionSource.prepareStatement("REPLACE INTO tableAuditorChanges "
+        try (PreparedStatement stmt = connectionSource.prepareStatement("REPLACE INTO tableAuditorChanges "
                 + "('id', 'blockId', 'networkId', 'operation', 'sequenceNum', 'oldValue', 'newValue') "
-                + "VALUES(?, ?, ?, ?, ?, ?, ?)");
+                + "VALUES(?, ?, ?, ?, ?, ?, ?)")) {
             int i = 0;
             stmt.setObject(++i, change.id);
             stmt.setObject(++i, change.blockId);
@@ -75,21 +60,16 @@ public class AuditorChangesRepository {
             stmt.setObject(++i, Utils.encodeHex(change.oldValue));
             stmt.setObject(++i, Utils.encodeHex(change.newValue));
             stmt.execute();
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
     }
-    
+
     public AuditorChangeData get(Long id) throws SQLException {
         List<AuditorChangeData> values = new ArrayList<AuditorChangeData>();
-        PreparedStatement stmt = null;
-        try {
-            stmt = connectionSource.prepareStatement("SELECT * FROM tableAuditorChanges WHERE id = ?");
+        try (PreparedStatement stmt = connectionSource.prepareStatement("SELECT * FROM tableAuditorChanges WHERE id = ?")) {
             int i = 0;
             stmt.setObject(++i, id);
             ResultSet resultSet = stmt.executeQuery();
-    
+
             while (resultSet.next()) {
                 AuditorChangeData data = new AuditorChangeData();
                 data.id = resultSet.getLong("id");
@@ -99,41 +79,31 @@ public class AuditorChangesRepository {
                 data.sequenceNum = resultSet.getInt("sequenceNum");
                 data.oldValue = Utils.decodeHex(resultSet.getString("oldValue"));
                 data.newValue = Utils.decodeHex(resultSet.getString("newValue"));
-    
+
                 values.add(data);
             }
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
 
-        if(values.size() > 1) throw new SQLException("Not an unique id: " + id);
-        if(values.size() == 0) return null;
+        if (values.size() > 1) throw new SQLException("Not an unique id: " + id);
+        if (values.size() == 0) return null;
         return values.get(0);
     }
-    
+
     public void delete(String blockId) throws SQLException {
-        PreparedStatement stmt = null;
-        try {
-            stmt = connectionSource.prepareStatement("DELETE FROM tableAuditorChanges WHERE blockId = ?");
+        try (PreparedStatement stmt = connectionSource.prepareStatement("DELETE FROM tableAuditorChanges WHERE blockId = ?")) {
             int i = 0;
             stmt.setObject(++i, blockId);
             stmt.execute();
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
     }
 
     public List<AuditorChangeData> getWithBlockId(String blockId) throws SQLException {
         List<AuditorChangeData> values = new ArrayList<AuditorChangeData>();
-        PreparedStatement stmt = null;
-        try {
-            stmt = connectionSource.prepareStatement("SELECT * FROM tableAuditorChanges WHERE blockId = ?");
+        try (PreparedStatement stmt = connectionSource.prepareStatement("SELECT * FROM tableAuditorChanges WHERE blockId = ?")) {
             int i = 0;
             stmt.setObject(++i, blockId);
             ResultSet resultSet = stmt.executeQuery();
-    
+
             while (resultSet.next()) {
                 AuditorChangeData data = new AuditorChangeData();
                 data.id = resultSet.getLong("id");
@@ -143,12 +113,9 @@ public class AuditorChangesRepository {
                 data.sequenceNum = resultSet.getInt("sequenceNum");
                 data.oldValue = Utils.decodeHex(resultSet.getString("oldValue"));
                 data.newValue = Utils.decodeHex(resultSet.getString("newValue"));
-    
+
                 values.add(data);
             }
-        } finally {
-            if(stmt != null) stmt.close();
-            stmt = null;
         }
         return values;
     }
