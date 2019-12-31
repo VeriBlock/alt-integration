997a121dc2bc40e8a33d27847865e0f54d6d07cf
Eugene Iegorochkin <eugene_iegorochkin@adoriasoft.com>
2019-12-31 13:55:18 +0200

    mock-pop-mining: fix VeriBlockBlockData to use getId() in Merkle path and root calculations; reimplement it to store regular and PoP VeriBlock transactions instead of blobs

--- mock-pop-mining/src/main/java/org/veriblock/sdk/mock/VeriBlockBlockData.java
+++ mock-pop-mining/src/main/java/org/veriblock/sdk/mock/VeriBlockBlockData.java
@@ -10,29 +10,90 @@ package org.veriblock.sdk.mock;
 
 import org.veriblock.sdk.models.Sha256Hash;
 import org.veriblock.sdk.models.VeriBlockMerklePath;
+import org.veriblock.sdk.models.VeriBlockPoPTransaction;
+import org.veriblock.sdk.models.VeriBlockTransaction;
+import org.veriblock.sdk.services.SerializeDeserializeService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VeriBlockBlockData {
 
-    List<byte[]> txs = new ArrayList<>();
-    List<byte[]> popTxs = new ArrayList<>();
+    private abstract class Subtree<T> extends ArrayList<T> {
+        abstract public Sha256Hash getSubject(int index);
+
+        public Sha256Hash getMerkleRoot() {
+            return calculateSubtreeHash(0, 0);
+        }
+
+        // calculate the number of bits it takes to store size()
+        private int getMaxDepth() {
+            return (int)(Math.log(size()) / Math.log(2) + 1);
+        }
+
+        // at each depth, there are 2**depth subtrees
+        // leaves are at the depth equal to getMaxDepth()
+        private Sha256Hash calculateSubtreeHash(int index, int depth) {
+            if (depth >= getMaxDepth()) {
+                return index < size() ? getSubject(index) : Sha256Hash.of(new byte[0]);
+            }
+
+            return Sha256Hash.of(calculateSubtreeHash(index * 2, depth + 1).getBytes(),
+                                 calculateSubtreeHash(index * 2 + 1, depth + 1).getBytes());
+        }
+
+        public List<Sha256Hash> getMerkleLayers(int index) {
+            if (index >= size())
+                throw new IndexOutOfBoundsException("index must be less than size()");
+
+            int maxDepth = getMaxDepth();
+            int layerIndex = index;
+
+            // 2 layers will be added by get*MerklePath()
+            List<Sha256Hash> layers = new ArrayList<>(maxDepth + 2);
+
+            for (int depth = maxDepth; depth > 0; depth--) {
+                // invert the last bit of layerIndex to reach the opposite subtree
+                Sha256Hash layer = calculateSubtreeHash(layerIndex ^ 1, depth);
+                layers.add(layer);
+
+                layerIndex /= 2;
+            }
+
+            return layers;
+        }
+
+    }
+
+    public class RegularSubtree extends Subtree<VeriBlockTransaction> {
+        public Sha256Hash getSubject(int index) {
+            return SerializeDeserializeService.getId(get(index));
+        }
+    }
+
+    public class PoPSubtree extends Subtree<VeriBlockPoPTransaction> {
+        public Sha256Hash getSubject(int index) {
+            return SerializeDeserializeService.getId(get(index));
+        }
+    }
+
+    RegularSubtree txs = new RegularSubtree();
+    PoPSubtree popTxs = new PoPSubtree();
 
     byte[] blockContentMetapackage = new byte[0];
 
     public Sha256Hash getMerkleRoot() {
         return Sha256Hash.of(
                     Sha256Hash.of(blockContentMetapackage).getBytes(),
-                    Sha256Hash.of(getMerkleRoot(txs).getBytes(),
-                                  getMerkleRoot(popTxs).getBytes()).getBytes());
+                    Sha256Hash.of(txs.getMerkleRoot().getBytes(),
+                                  popTxs.getMerkleRoot().getBytes()).getBytes());
     }
 
-    public List<byte[]> getRegularTransactions() {
+    public RegularSubtree getRegularTransactions() {
         return txs;
     }
 
-    public List<byte[]> getPoPTransactions() {
+    public PoPSubtree getPoPTransactions() {
         return popTxs;
     }
 
@@ -44,70 +105,30 @@ public class VeriBlockBlockData {
         return blockContentMetapackage;
     }
 
-    private VeriBlockMerklePath getMerklePath(int treeIndex, int index) {
-        assert(treeIndex == 0 || treeIndex == 1);
+    public VeriBlockMerklePath getRegularMerklePath(int index) {
+        Sha256Hash subject = txs.getSubject(index);
 
-        List<byte[]> transactions = treeIndex == 0 ? txs : popTxs;
-
-        Sha256Hash subject = Sha256Hash.of(transactions.get(index));
-
-        List<Sha256Hash> layers = getMerkleLayers(index, transactions);
+        List<Sha256Hash> layers = txs.getMerkleLayers(index);
 
         // the other transaction subtree
-        layers.add(getMerkleRoot(treeIndex == 1 ? txs : popTxs));
+        layers.add(popTxs.getMerkleRoot());
 
         layers.add(Sha256Hash.of(blockContentMetapackage));
 
-        return new VeriBlockMerklePath(treeIndex, index, subject, layers);
-    }
-
-    public VeriBlockMerklePath getRegularMerklePath(int index) {
-        return getMerklePath(0, index);
+        return new VeriBlockMerklePath(0, index, subject, layers);
     }
 
     public VeriBlockMerklePath getPoPMerklePath(int index) {
-        return getMerklePath(1, index);
-    }
+        Sha256Hash subject = popTxs.getSubject(index);
 
-    private Sha256Hash getMerkleRoot(List<byte[]> txs) {
-        return calculateSubtreeHash(0, 0, txs);
-    }
+        List<Sha256Hash> layers =  popTxs.getMerkleLayers(index);
 
-    // calculate the number of bits it takes to store size()
-    private int getMaxDepth(List<byte[]> txs) {
-        return (int)(Math.log(txs.size()) / Math.log(2) + 1);
-    }
+        // the other transaction subtree
+        layers.add(txs.getMerkleRoot());
 
-    // at each depth, there are 2**depth subtrees
-    // leaves are at the depth equal to getMaxDepth()
-    private Sha256Hash calculateSubtreeHash(int index, int depth, List<byte[]> txs) {
-        if (depth >= getMaxDepth(txs)) {
-            return Sha256Hash.of(index < txs.size() ? txs.get(index) : new byte[0]);
-        }
+        layers.add(Sha256Hash.of(blockContentMetapackage));
 
-        return Sha256Hash.of(calculateSubtreeHash(index * 2, depth + 1, txs).getBytes(),
-                             calculateSubtreeHash(index * 2 + 1, depth + 1, txs).getBytes());
-    }
-
-    private List<Sha256Hash> getMerkleLayers(int index, List<byte[]> txs) {
-        if (index >= txs.size())
-            throw new IndexOutOfBoundsException("index must be less than size()");
-
-        int maxDepth = getMaxDepth(txs);
-        int layerIndex = index;
-
-        // 2 layers will be added by get*MerklePath()
-        List<Sha256Hash> layers = new ArrayList<>(maxDepth + 2);
-
-        for (int depth = maxDepth; depth > 0; depth--) {
-            // invert the last bit of layerIndex to reach the opposite subtree
-            Sha256Hash layer = calculateSubtreeHash(layerIndex ^ 1, depth, txs);
-            layers.add(layer);
-
-            layerIndex /= 2;
-        }
-
-        return layers;
+        return new VeriBlockMerklePath(1, index, subject, layers);
     }
 
 }
