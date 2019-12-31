5be45751b0547a3395d5a9dedb4f854c2aeced10
eu321 <52827508+eu321@users.noreply.github.com>
2019-12-31 14:20:25 +0200

    mock-pop-mining: finalize VeriBlock mining; set keystones correctly (#101)

--- mock-pop-mining/src/test/java/org/veriblock/sdk/mock/VeriBlockBlockchainTest.java
+++ mock-pop-mining/src/test/java/org/veriblock/sdk/mock/VeriBlockBlockchainTest.java
@@ -22,22 +22,10 @@ import org.junit.Test;
 import org.veriblock.sdk.VeriBlockSecurity;
 import org.veriblock.sdk.blockchain.store.BitcoinStore;
 import org.veriblock.sdk.blockchain.store.VeriBlockStore;
-import org.veriblock.sdk.conf.MainNetParameters;
-import org.veriblock.sdk.models.Sha256Hash;
-import org.veriblock.sdk.models.VBlakeHash;
 import org.veriblock.sdk.models.VeriBlockBlock;
-import org.veriblock.sdk.models.VerificationException;
-import org.veriblock.sdk.services.SerializeDeserializeService;
 import org.veriblock.sdk.sqlite.ConnectionSelector;
-import org.veriblock.sdk.util.Utils;
 
 public class VeriBlockBlockchainTest {
-
-    // FIXME: need a proper genesis block
-    private final byte[] rawGenesis = Utils.decodeHex("0001998300029690ACA425987B8B529BEC04654A16FCCE708F3F0DEED25E1D2513D05A3B17C49D8B3BCFEFC10CB2E9C4D473B2E25DB7F1BD040098960DE0E313");
-    private final VeriBlockBlock genesis = SerializeDeserializeService.parseVeriBlockBlock(rawGenesis);
-        
-    private final List<VeriBlockBlock> bootstrap = Arrays.asList(genesis);
 
     private VeriBlockSecurity security;
     private org.veriblock.sdk.blockchain.VeriBlockBlockchain blockchain;
@@ -50,10 +38,9 @@ public class VeriBlockBlockchainTest {
 
         blockchain = security.getVeriBlockBlockchain();
 
-
         VeriBlockStore veriBlockStore = new VeriBlockStore(ConnectionSelector.setConnectionInMemory());
         BitcoinStore bitcoinStore = new BitcoinStore(ConnectionSelector.setConnectionInMemory());
-        mockchain = new VeriBlockBlockchain(new MainNetParameters(), veriBlockStore, bitcoinStore);
+        mockchain = new VeriBlockBlockchain(VeriBlockDefaults.networkParameters, veriBlockStore, bitcoinStore);
     }
 
     @After
@@ -66,16 +53,12 @@ public class VeriBlockBlockchainTest {
         // FIXME: turn on difficulty checking after refactoring the calculator
         blockchain.setSkipValidateBlocksDifficulty(true);
         
-        mockchain.bootstrap(bootstrap);
-        blockchain.bootstrap(bootstrap);
-        
-        // FIXME: fix mining
-        try {
+        mockchain.bootstrap(VeriBlockDefaults.bootstrap);
+        blockchain.bootstrap(VeriBlockDefaults.bootstrap);
+
+        for (int i = 0; i < 50; i++) {
             VeriBlockBlock block = mockchain.mine(new VeriBlockBlockData());
             blockchain.add(block);
-            Assert.fail("Expected VerificationException");
-        } catch(VerificationException e) {
-            Assert.assertTrue(e.getMessage().startsWith("Block hash is higher than target"));
         }
     }
 }
